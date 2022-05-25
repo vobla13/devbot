6c616bf6354cacf2ad40ba6be0d7a2015566eaa7
Vadim Kim <v.kim@rambler-co.ru>
2022-05-25 18:10:42 +0300

    Add inline keyboard. Some refactoring.

--- devbot.py
+++ devbot.py
@@ -4,11 +4,12 @@ from telebot import types, TeleBot
 
 
 
-
+# Костыль: перейти на getenv
 token = helpers.get_bot_token()
 bot: TeleBot = telebot.TeleBot(token)
 
 
+# Ответ на команду /start
 @bot.message_handler(commands=['start'])
 def start(message, res=False):
     if message.from_user.username != '':
@@ -22,22 +23,72 @@ def start(message, res=False):
                                       f'free dev - освободить dev\n')
 
 
+# Получить статусы по всем dev в одном сообщении командой /status
 @bot.message_handler(commands=['status'])
 def status(message, res=False):
-    bot.send_message(message.chat.id, helpers.get_all_status())
+    answer = helpers.get_all_status()
+    bot.send_message(message.chat.id, answer)
 
 
+# Занять dev по сообщению 'take dev(номер)'разбирается регуляркой
 @bot.message_handler(regexp='[Tt]ake [Dd]ev[0-9]')
 def take_dev(message, res=False):
     dev = message.text[5:].lower()
-    bot.send_message(message.chat.id, helpers.set_dev_user(dev, f'@{message.from_user.username}'))
+    # Помимо пользователя и бота в логику вклинивается еще третья сторона - текущий пользователь dev (если он ранее был занят)
+    # Нам небходимо сохранять правильные username и chat_id и корректно передавать их в методы
+    # Сохраняем user_name и chat_id запрашивающего (candidate)
+    candidate_username = message.from_user.username
+    candidate_chat_id = message.chat.id
+    
+    # Проверяем, что dev существует
+    if helpers.get_dev_status(dev) is False:
+        bot.send_message(candidate_chat_id, f'{dev} не существует!')
+
+    # Проверяем, что dev занят
+    # Если False, то записываем запращивающего хозяином и возвращаем ему ответ с успешным статусом
+    elif helpers.check_dev_busy(dev) is False:
+        answer = helpers.set_dev_user(dev, candidate_username, candidate_chat_id)    
+        bot.send_message(candidate_chat_id, answer)
+    
+    # Если True и там кто-то уже есть.
+    else:
+        # Запрашиваем user_name и chat_id текущего хозяина и записываем их в переменные
+        dev_username, dev_user_chat_id = helpers.get_dev_user(dev)
+        # Пишем запрашивающему сообщение о том, что dev занят и кем
+        bot.send_message(candidate_chat_id, f'{dev} занят @{dev_username}. Запрашиваю разрешение...')
+        
+        # Создаем инлайн клавиатуру
+        buttons = ['Yes', 'No']
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*buttons)
+        # Посылаем запрос текущему пользователю
+        bot.send_message(dev_user_chat_id, f'@{candidate_username} хочет отобрать у тебя {dev}!', reply_markup=keyboard)
+        
+        # Создаем обработчики кнопок
+        @bot.message_handler(content_types='text')
+        def button_reply(message, res=False):
+            if message.text=="Yes":
+                # Если ДА, то освобождаем dev и заново устанавливаем нового пользователя
+                # helpers.free_dev(dev)
+                bot.send_message(dev_user_chat_id, f'Cпасибо :)', reply_markup=types.ReplyKeyboardRemove())
+                bot.send_message(candidate_chat_id, f'@{dev_username} разрешил')
+                # answer = helpers.set_dev_user(dev, message.from_user.username, message.chat.id)
+                # bot.send_message(dev_user_chat_id, answer)
+            elif message.text=="No":
+                # Если НЕТ, то отправляем отказ
+                bot.send_message(dev_user_chat_id, f'Грусть, печаль, тоска', reply_markup=types.ReplyKeyboardRemove())
+                bot.send_message(candidate_chat_id, f'@{dev_username} отказал. Напишите ему.')
 
 
+# Освободить dev по сообщению 'free dev(номер)' разбирается регуляркой
 @bot.message_handler(regexp='[Ff]ree [Dd]ev[0-9]')
 def free_dev(message, res=False):
     dev = message.text[5:].lower()
     result = helpers.free_dev(dev)
-    bot.send_message(message.chat.id, result)
+    bot.send_message(message.chat.id, result, reply_to_message_id=message.message_id)
 
 
-bot.polling()
+# Запускаем бота
+# Можно использовать try/except с бесконечным циклом, но есть риск попасть в infinityloop
+# Возможно, решается таской, которая будет перезапускать бота при падении
+bot.infinity_polling(interval=0, timeout=20)
--- helpers.py
+++ helpers.py
@@ -14,71 +14,85 @@ def timedelta_format(duration):
 
 
 # Setup devs and their status
+# Костыль: перейти на класс и передавать объект класса в словарь
+# Хранить состояение классов в JSON и поднимать при перезапуске бота
 devs = {
-        'dev2': {'user': 'free', 'time': datetime.now()},
-        'dev3': {'user': 'free', 'time': datetime.now()},
-        'dev4': {'user': 'free', 'time': datetime.now()},
-        'dev5': {'user': 'free', 'time': datetime.now()},
-        'dev6': {'user': 'free', 'time': datetime.now()},
-        'dev7': {'user': 'free', 'time': datetime.now()},
-        'dev8': {'user': 'free', 'time': datetime.now()},
-        'dev9': {'user': 'free', 'time': datetime.now()},
-        'dev10': {'user': 'free', 'time': datetime.now()},
-        'dev11': {'user': 'free', 'time': datetime.now()},
-        'dev12': {'user': 'free', 'time': datetime.now()}
+        'dev2': {'user': 'free', 'chat_id:': 0, 'time': datetime.now()},
+        'dev3': {'user': 'free', 'chat_id:': 0, 'time': datetime.now()},
+        'dev4': {'user': 'free', 'chat_id:': 0, 'time': datetime.now()},
+        'dev5': {'user': 'free', 'chat_id:': 0, 'time': datetime.now()},
+        'dev6': {'user': 'free', 'chat_id:': 0, 'time': datetime.now()},
+        'dev7': {'user': 'free', 'chat_id:': 0, 'time': datetime.now()},
+        'dev8': {'user': 'free', 'chat_id:': 0, 'time': datetime.now()},
+        'dev9': {'user': 'free', 'chat_id:': 0, 'time': datetime.now()},
+        'dev10': {'user': 'free', 'chat_id:': 0, 'time': datetime.now()},
+        'dev11': {'user': 'free', 'chat_id:': 0, 'time': datetime.now()},
+        'dev12': {'user': 'free', 'chat_id:': 0, 'time': datetime.now()}
     }
 
 
 # Get tg_bot token
+# Костыль: заменить на os.getenv и библиотеку dotenv, и передавать в нее токен
 def get_bot_token():
     f = open('setup', mode='r', encoding='UTF-8')
-    token = f.read().split('\n')
+    token = f.read()
     f.close()
-    return token[0]
+    return token
 
 
-# Get <dev>, <user> and user <timedelta> in HH:MM format by <dev>
-def get_dev_status(dev):
+# Get <user_name>, <chat_id> by <dev>
+def get_dev_user(dev):
     current_dev = devs.get(dev)
     if current_dev is not None:
         user = current_dev.get('user')
-        d, h, m, s = timedelta_format(datetime.now() - current_dev.get('time'))
-        return f'{dev} | {user} (+{h}h {m}m)'
+        chat_id = current_dev.get('chat_id')
+        return user, chat_id
     else:
-        return f'{dev} is not available.'
+        return None
+
+
+# Get <dev> status
+def get_dev_status(dev):
+    current_dev = devs.get(dev)
+    if current_dev is not None:
+        d, h, m, s = timedelta_format(datetime.now() - current_dev.get('time'))
+        user = current_dev.get('user')
+        time = f'(+{h}h {m}m)'
+        return user, time
+    else:
+        return False
 
 
 # Returns all <dev> status
 def get_all_status():
     result = ''
     for dev in devs:
-        result += get_dev_status(dev) + '\n'
+        user, time = get_dev_status(dev)
+        result += f'{dev} | @{user} {time}\n'
     return result
 
 
-# Check <dev> for existing <user>: returns False if <user> is not 'free'
-def check_dev_user(dev):
+# Check <dev> for existing <user>: returns True if <user> is not 'free'
+def check_dev_busy(dev):
     current_dev = devs.get(dev)
     if current_dev is not None:
         if current_dev['user'] != 'free':
+            return True
+        else:
             return False
-    else:
-        return f'{dev} is busy.'
 
 
 # Set <user> to <dev>: returns <dev> status if <dev> have existing <user>
-def set_dev_user(dev, user):
+# Порефакторить
+def set_dev_user(dev, user, chat_id):
     current_dev = devs.get(dev)
     if current_dev is not None:
-        if check_dev_user(dev) is not False:
-            current_dev['user'] = user
-            current_dev['time'] = datetime.now()
-            return f'{user} -> {dev}'
-        else:
-            current_user = current_dev['user']
-            return f'FAILED. Is busy by {current_user}.'
+        current_dev['user'] = user
+        current_dev['time'] = datetime.now()
+        current_dev['chat_id'] = chat_id
+        return f'{dev} занял @{user}'
     else:
-        return f'{dev} is not available.'
+        return f'{dev} не существует!'
 
 
 # Reset <dev> status to "Free"
@@ -87,7 +101,7 @@ def free_dev(dev):
     if current_dev is not None:
         current_dev['user'] = 'free'
         current_dev['time'] = datetime.now()
-        return f'{dev} is free now!'
+        current_dev['chat_id'] = 0
+        return f'{dev} теперь свободен!'
     else:
-        return f'{dev} is not available.'
-
+        return f'{dev} не существует!'
