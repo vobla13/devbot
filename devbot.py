e596f3f6312d25a3b1d6a1270c38f1105d89c109
Gitcha <vadkim.work@gmail.com>
2022-05-29 01:57:30 +0300

    Fix callbacks handler.

--- devbot.py
+++ devbot.py
@@ -1,6 +1,6 @@
 import telebot
 import helpers
-from telebot import types, TeleBot
+from telebot import TeleBot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
@@ -8,15 +8,6 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 # Костыль: перейти на getenv
 token = helpers.get_bot_token()
 bot: TeleBot = telebot.TeleBot(token)
-
-
-# Создаем объект Inline-клавиатуры
-def gen_markup():
-    markup = InlineKeyboardMarkup()
-    markup.row_width = 2
-    markup.add(InlineKeyboardButton("Yes", callback_data="cb_yes"),
-               InlineKeyboardButton("No", callback_data="cb_no"))
-    return markup
 
 
 # Ответ на команду /start
@@ -47,41 +38,58 @@ def take_dev(message):
     candidate_chat_id = message.chat.id
     dev = message.text[5:].lower()
     dev_username, dev_user_chat_id = helpers.get_dev_user(dev)
-
-    # Помимо пользователя и бота в логику вклинивается еще третья сторона - текущий пользователь dev (если он ранее был занят)
-    # Нам небходимо сохранять правильные username и chat_id и корректно передавать их в методы
-    # Сохраняем user_name и chat_id запрашивающего (candidate)
     
     # Проверяем, что dev существует
     if helpers.get_dev_status(dev) is False:
         bot.reply_to(message, f'{dev} не существует!')
+    
     # Проверяем, что dev занят
     # Если False, то записываем запрашивающего хозяином и возвращаем ему ответ с успешным статусом
     elif helpers.check_dev_busy(dev) is False:
         answer = helpers.set_dev_user(dev, candidate_username, candidate_chat_id)    
         bot.reply_to(message, answer)
+    
     # Если True и там кто-то уже есть.
     else:
         # Пишем запрашивающему сообщение о том, что dev занят и кем
         bot.reply_to(message, f'{dev} занят @{dev_username}. Запрашиваю разрешение...')
+        # Создаем объект Inline-клавиатуры
+        
+        def gen_markup():
+            markup = InlineKeyboardMarkup()
+            markup.row_width = 2
+            # В данных колбеков передаем команду и данные кандидата
+            markup.add(InlineKeyboardButton("Yes", callback_data=f'yes_{dev}_{candidate_username}_{candidate_chat_id}'),
+                       InlineKeyboardButton("No", callback_data=f'no_{dev}_{candidate_username}_{candidate_chat_id}'))
+            markup.one_time_keyboard = True
+            return markup
+
+
         # Создаем обработчик колбеков нажатий
         @bot.callback_query_handler(func=lambda call: True)
         def callback_query(call):
-            if call.data == "cb_yes":
+            # Прокидываем данные из колбека внутрь
+            action = call.data.split("_")[0]
+            dev = call.data.split("_")[1]
+            candidate_username = call.data.split("_")[2]
+            candidate_chat_id = call.data.split("_")[3]
+            
+            if action == 'yes':
                 bot.answer_callback_query(call.id, "Спасибо :)")
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f'Отдал {dev} -> @{candidate_username}')
                 helpers.free_dev(dev)
-                # helpers.set_dev_user(dev, candidate_username, candidate_chat_id)
-                bot.send_message(candidate_chat_id, f'from: {dev_username} - Разрешено')
-            elif call.data == "cb_no":
-                bot.answer_callback_query(call.id, "А ты жадный :)")
-                bot.send_message(candidate_chat_id, f'from: {dev_username} - Отказано!')
-        
-        # Отправляем запрос хозяину dev
-        # Проблема при смене ролей - перестают правильно разруливаться сообщения "Разрешено / Отказано"
-        # Дебаг выявил проблему, что переменные dev, candidate_username, candidate_chat_id после первого круга фиксируются
-        # Подумать, как прокидывать переменные в другие переменные и переиспользовать их внутри отдельных кусков функции
-        bot.send_message(dev_user_chat_id, f'@{candidate_username} хочет отобрать у тебя dev!', reply_markup=gen_markup())
+                helpers.set_dev_user(dev, candidate_username, candidate_chat_id)
+                bot.send_message(candidate_chat_id, f'Ты забрал {dev}')
+            else:
+                bot.answer_callback_query(call.id, "Ну ладно :)")
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f'Оставил {dev}')
+                # bot.edit_message_reply_markup(call.from_user.id, call.message.message_id, reply_markup=())
+                bot.send_message(candidate_chat_id, f'Подожди, {dev} еще нужен!')
 
+
+        # Отправляем запрос хозяину dev
+        bot.send_message(dev_user_chat_id, f'@{candidate_username} хочет отобрать у тебя {dev}!', reply_markup=gen_markup())
+        
 
 
 # Освободить dev по сообщению 'free dev(номер)' разбирается регуляркой
